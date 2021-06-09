1862dbee1b68aeaac6b00460e2f7e1c75ba6900c
reazul7 <islamreazul7777@gmail.com>
2021-06-09 21:37:20 +0600

    content style call index.css

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,18 +5,18 @@ import imageTwo from "./images/egg-2.jpg";
 const Content = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+      <div className="menu-card">
         <img src={imageOne} alt="" className="h-full rounded mb-20 shadow" />
-        <div className="flex flex-col justify-center items-center">
+        <div className="center-content">
           <h2 className="text-2xl mb-2">Egg Muffins</h2>
           <p className="mb-2">Crispy, delicious, and nutritious</p>
           <span>$16</span>
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center bg-white h-screen font-mono py-40">
+      <div className="menu-card">
         <img src={imageTwo} alt="" className="h-full rounded mb-20 shadow" />
-        <div className="flex flex-col justify-center items-center">
+        <div className="center-content">
           <h2 className="text-2xl mb-2">Egg Salad</h2>
           <p className="mb-2">Crispy, delicious, and nutritious</p>
           <span>$18</span>
