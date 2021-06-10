45659e09af1797b0bddd74d52b4aec2b98243721
reazul7 <islamreazul7777@gmail.com>
2021-06-10 16:09:23 +0600

    add dropdown

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -10,11 +11,19 @@ import Footer from './components/Footer';
 import Home from './components/pages/index';
 import Menu from './components/pages/menu';
 import About from './components/pages/about';
+import Dropdown from './components/Dropdown';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <>
-      <NavBar/>
+      <NavBar toggle={toggle}/>
+      <Dropdown isOpen={isOpen} toggle={toggle}/>
       <Switch>
         <Route path="/" exact component={Home}/>
         <Route path="/menu" exact component={Menu}/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const NavBar = () => {
+const NavBar = ({toggle}) => {
     return (
         <nav className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono" role="navigation">
             <Link to='/' className="pl-8">EGG</Link>
-            <div className="px-4 cursor-pointer md:hidden">
+            <div className="px-4 cursor-pointer md:hidden"
+                onClick={toggle}
+            >
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
