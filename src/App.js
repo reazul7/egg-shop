902540e7177112dcc4c05e4c6276d5cda24cb5dc
reazul7 <islamreazul7777@gmail.com>
2021-06-10 16:34:56 +0600

    dropdown fix

--- src/App.js
+++ src/App.js
@@ -20,6 +20,20 @@ function App() {
     setIsOpen(!isOpen);
   }
 
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false)
+        console.log('dropdown resized')
+      }
+    }
+
+    window.addEventListener('resize', hideMenu)
+    return () => {
+      window.removeEventListener('resize', hideMenu);
+    }
+  })
+
   return (
     <>
       <NavBar toggle={toggle}/>
